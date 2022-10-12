418ee73608237209745945ab0d647b8ba51af926
tarekbrahmi <brahmitarek6@gmail.com>
2022-10-12 16:24:57 +0100

    line detection and other projects

---
+++ Open-cv-detections/line-detect/HoughLine-opencv.py
@@ -0,0 +1,30 @@
+import cv2
+import numpy as np
+img = cv2.imread('./line.jpg')
+
+# Convert the img to grayscale
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+# Apply edge detection method on the image
+edges = cv2.Canny(gray, 50, 150, apertureSize=3)
+lines_list = []
+lines = cv2.HoughLinesP(
+    edges,  # Input edge image
+    1,  # Distance resolution in pixels
+    np.pi/180,  # Angle resolution in radians
+    threshold=100,  # Min number of votes for valid line
+    minLineLength=5,  # Min allowed length of line
+    maxLineGap=10  # Max allowed gap between line for joining them
+)
+
+# Iterate over points
+for points in lines:
+    # Extracted points nested in the list
+    x1, y1, x2, y2 = points[0]
+    # Draw the lines joing the points
+    # On the original image
+    cv2.line(img, (x1, y1), (x2, y2), (0, 255, 0), 2)
+    # Maintain a simples lookup list for points
+    lines_list.append([(x1, y1), (x2, y2)])
+print('line list', lines_list)
+cv2.imwrite('linesDetected.jpg', img)
