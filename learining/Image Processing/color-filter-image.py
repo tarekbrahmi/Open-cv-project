0e87c0469d02cdb5f15921d4b96eabb91c2eae0f
tarekbrahmi <brahmitarek6@gmail.com>
2022-10-14 15:59:49 +0100

    make two mask for red color

--- learining/Image Processing/color-filter-image.py
+++ learining/Image Processing/color-filter-image.py
@@ -2,28 +2,31 @@ import cv2
 import numpy as np
 
 cap = cv2.VideoCapture(0)
+i = 0
+while (i < 5):
+    _, frame = cap.read()
+    # It converts the BGR color space of image to HSV color space
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    lower1 = np.array([0, 100, 20])
+    upper1 = np.array([10, 255, 255])
+    # upper boundary RED color range values; Hue (160 - 180)
+    lower2 = np.array([160, 100, 20])
+    upper2 = np.array([179, 255, 255])
+    # preparing the mask to overlay
+    mask1 = cv2.inRange(hsv, lower1, upper1)
+    mask2 = cv2.inRange(hsv, lower2, upper2)
+    mask = mask1+mask2
 
-while(1):
-	_, frame = cap.read()
-	# It converts the BGR color space of image to HSV color space
-	hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-	
-	# Threshold of blue in HSV space
-	lower_blue = np.array([60, 35, 140])
-	upper_blue = np.array([180, 255, 255])
+    # The black region in the mask has the value of 0,
+    # so when multiplied with original image removes all non-blue regions
+    result = cv2.bitwise_and(frame, frame, mask=mask)
 
-	# preparing the mask to overlay
-	mask = cv2.inRange(hsv, lower_blue, upper_blue)
-	
-	# The black region in the mask has the value of 0,
-	# so when multiplied with original image removes all non-blue regions
-	result = cv2.bitwise_and(frame, frame, mask = mask)
+    cv2.imshow('frame', frame)
+    cv2.imshow('mask', mask)
+    cv2.imshow('result', result)
 
-	cv2.imshow('frame', frame)
-	cv2.imshow('mask', mask)
-	cv2.imshow('result', result)
-	
-	cv2.waitKey(0)
+    cv2.waitKey(0)
+    i += 1
 
 cv2.destroyAllWindows()
 cap.release()
