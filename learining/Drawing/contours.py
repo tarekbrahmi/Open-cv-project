b6e0f2cff13f565e5465d5ea0780694711e1f5db
tarekbrahmi <brahmitarek6@gmail.com>
2022-10-24 22:02:44 +0100

    remove some comments

--- learining/Drawing/contours.py
+++ learining/Drawing/contours.py
@@ -2,25 +2,14 @@ import cv2
 
 image = cv2.imread('contours2.jpg')
 
-
-# Grayscale
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-
-# Find Canny edges
 edged = cv2.Canny(gray, 30, 200)
-
-# Finding Contours
-# Use a copy of the image e.g. edged.copy()
-# since findContours alters the image
 contours, hierarchy = cv2.findContours(image=edged,
                                        mode=cv2.RETR_EXTERNAL,
                                        method=cv2.CHAIN_APPROX_NONE)
 
 
 print("Number of Contours found = " + str(len(contours)))
-
-# Draw all contours
-# -1 signifies drawing all contours
 cv2.drawContours(image=image, contours=contours,
                  contourIdx=-1, color=(0, 255, 0), thickness=3)
 
--- learining/Drawing/draw-circle.py
+++ learining/Drawing/draw-circle.py
@@ -1,30 +1,14 @@
 import cv2
 import numpy as np
 	
-
-# Reading an image in default mode
 Img = np.zeros((512, 512, 3), np.uint8)
-	
-# Window name in which image is displayed
+
 window_name = 'Image'
-
-# Center coordinates
 center_coordinates = (Img.shape[1]//2, Img.shape[0]//2)
-
-# Radius of circle
 radius = 100
-
-# Blue color in BGR
 color = (255, 0, 0)
-
-# Line thickness of 1 px
 thickness = 1
-
-# Using cv2.circle() method
-# Draw a circle of red color of thickness -1 px
 image = cv2.circle(Img, center_coordinates, radius, color, thickness)
-
-# Displaying the image
 cv2.imshow(window_name, image)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
