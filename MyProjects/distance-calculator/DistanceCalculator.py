685a4ba7e4a4a1134d126c36ff52213a6c77364f
tarekbrahmi <brahmitarek6@gmail.com>
2022-10-28 16:58:45 +0100

    fixing

--- MyProjects/distance-calculator/DistanceCalculator.py
+++ MyProjects/distance-calculator/DistanceCalculator.py
@@ -9,14 +9,23 @@ HAARCASCADE_PATH = "haarcascade_frontalface_default.xml"
 
 class DistanceCalculator:
     FACE_WIDTH = 0
+    REF_FACE_WIDTH = 14  # face width
+    REF_FACE_DISTANCE = 20  # face-webcam distance
     (width, height) = (130, 100)
+
+    def focal_ref(self, width_image_ref):
+        return (width_image_ref * self.REF_FACE_DISTANCE) / self.REF_FACE_WIDTH
+
+    def focal_real(self, width_image_real):
+
+        return (width_image_real * self.REF_FACE_DISTANCE) / self.REF_FACE_WIDTH
 
     def __init__(self, face_rec, names) -> None:
         self.face_detector = cv2.CascadeClassifier(HAARCASCADE_PATH)
         self.model = face_rec.model
         self.names = names
 
-    def face_ref_width(self, original):
+    def face_real_width(self, original):
         """ detect and draw green rectangle in face"""
         gray_scaled = cv2.cvtColor(src=original, code=cv2.COLOR_BGR2GRAY)
         faces = self.face_detector.detectMultiScale(gray_scaled, 1.3, 5)
@@ -34,13 +43,13 @@ class DistanceCalculator:
                             (self.names[prediction[0]],
                              prediction[1]), (x-10, y-10),
                             cv2.FONT_HERSHEY_PLAIN, 1, (0, 255, 0))
+                self.FACE_WIDTH = w
             else:
                 cv2.putText(original, 'not recognized',
                             (x-10, y-10), cv2.FONT_HERSHEY_PLAIN, 1, (0, 255, 0))
-            self.FACE_WIDTH = w
 
     def calc_distance(self, original):
-        self.face_ref_width(original=original)
+        self.face_real_width(original=original)
 
     def preproccess(self):
 
